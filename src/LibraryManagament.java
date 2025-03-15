929c153862d600975cd38ac3eb9b9033ad19e4b1
Abdullah Shabbir <ssabdullah_1992@hotmail.com>
2025-03-15 12:08:16 +1100

    Added Search book feature with repeat menu options

--- src/LibraryManagament.java
+++ src/LibraryManagament.java
@@ -9,23 +9,34 @@ public class LibraryManagament {
 
         System.out.println("Welcome to the Library Management System");
 
-        System.out.println("Enter 1 to add a book");
-        System.out.println("Enter 2 to search a book");
-        System.out.println("Enter 3 to display all books");
-        System.out.println("Enter 4 to exit");
-        System.out.print("Enter your choice: ");
+        while (true) {
+            System.out.println("Enter 1 to add a book");
+            System.out.println("Enter 2 to search a book");
+            System.out.println("Enter 3 to display all books");
+            System.out.println("Enter 4 to exit");
+            System.out.print("Enter your choice: ");
 
-        int choice = scanner.nextInt();
+            int choice = scanner.nextInt();
 
-        switch (choice){
-            case 1:
-                System.out.print("Enter the name of the book: ");
-                scanner.nextLine();
-                String bookName = scanner.nextLine();
-                books[bookCount] = bookName;
-                bookCount++;
-                System.out.println("Book added successfully");
-                break;
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter the name of the book: ");
+                    scanner.nextLine();
+                    String bookName = scanner.nextLine();
+                    books[bookCount] = bookName;
+                    bookCount++;
+                    System.out.println("Book added successfully\n");
+                    break;
+                case 3:
+                    System.out.println("List of all books:");
+
+                    for (int i = 0; i < bookCount; i++) {
+                        System.out.println(books[i]);
+                    }
+
+                    System.out.println();
+                    break;
+            }
         }
     }
 }
